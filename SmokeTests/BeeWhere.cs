049ddfbc7b3e64212bf980ce9bffb9b2cc72004c
stefan_mehlig <smehlig@calicosol.com>
2020-04-18 16:33:54 -0600

    BeeWhere county staff login

--- SmokeTests/BeeWhere.cs
+++ SmokeTests/BeeWhere.cs
@@ -159,7 +159,234 @@ namespace SmokeTests
             Assert.IsTrue(testResult);
         }
 
- 
+        [TestCategory("BeeWhere")]
+        [TestMethod]
+        public void TestBeekeeperMainPage()
+        {
+            testId = "5.2.1";
+            testTitle = "BeeWhere Beekeeper Main Page";
+
+            int stepNumber = 0;
+            string stepName = "";
+            bool stepResult = true;
+
+            bool testResult = true;
+            bool testAbort = false;
+
+            IWebElement webElement;
+
+            try
+            {
+                // STEP: open browser and navigate to login page
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Naviage to " + appURL + " and verify title";
+                    stepResult = true;
+                    //   action
+                    browser.Navigate().GoToUrl(appURL);
+                    Helper.TakeScreenshot(browser, testId, stepNumber);
+                    //   report
+                    stepResult = Helper.TestStepContains(stepNumber, stepName, "BeeWhere California", browser.Title);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = true;
+                    }
+                }
+
+                // STEP: check if Access Beekeeper Mgmt button is present
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Verify link 'Beekeeper' is present";
+                    stepResult = true;
+                    //   verify
+                    webElement = browser.FindElement(By.PartialLinkText("BEEKEEPER MANAGEMENT"));
+                    Helper.TestStepComment("Link '" + webElement.Text + "' goes to '" + webElement.GetAttribute("href") + "'.");
+                    //   report
+                    stepResult = webElement.Displayed;
+                    Helper.TestStepResult(stepNumber, stepName, stepResult);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+
+
+
+
+                // STEP: click on  Access Beekeeper button 
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Click button 'Beekeeper'";
+                    stepResult = true;
+                    //   action
+                    webElement = browser.FindElement(By.PartialLinkText("BEEKEEPER MANAGEMENT"));
+                    webElement.Click();
+                    //   verify
+                    stepResult = Helper.TestStepContains(stepNumber, stepName, "BeeWhere California", browser.Title);
+                    //   report
+                    Helper.TakeScreenshot(browser, testId, stepNumber);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+                // STEP: enter user credentials
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Enter user credentials";
+                    stepResult = true;
+                    //   action
+                    browser.FindElement(By.Id("Username")).SendKeys("calico51");
+                    browser.FindElement(By.Id("Password")).SendKeys("Denver5280!");
+                    Helper.TakeScreenshot(browser, testId, stepNumber);
+                    //   report
+                    Helper.TestStepResult(stepNumber, stepName, stepResult);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+                // STEP: click the Login button
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Click login button - Dashboard appears";
+                    stepResult = true;
+                    //   verify
+                    browser.FindElement(By.XPath("//input[@value='Log in']")).Click();
+                    stepResult = Helper.TestStepContains(stepNumber, stepName, "BeeWhere California | Index", browser.Title);
+                    //   report
+                    Helper.TakeScreenshot(browser, testId, stepNumber);
+                    Helper.TestStepResult(stepNumber, stepName, stepResult);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+
+                // STEP: check H2 tag
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Verify correct title (H2 tag)";
+                    stepResult = true;
+                    //   verify
+                    webElement = browser.FindElement(By.XPath("//h2"));
+                    //   report
+                    stepResult = Helper.TestStepCompare(stepNumber, stepName, "County Administration", webElement.Text);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+
+                // STEP: check if the name of the logged in person shows correctly
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Verify correct User Name displays";
+                    stepResult = true;
+                    //   verify
+                    // recorded xpath: //*[@id="logoutForm"]/div/div[1]
+                    webElement = browser.FindElement(By.XPath("//*[@id='logoutForm']/div/div[1]"));
+                    //   report
+                    stepResult = Helper.TestStepCompare(stepNumber, stepName, "calico51", webElement.Text);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+                // STEP: check if the name of the logged in person shows correctly
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Verify correct User Name displays";
+                    stepResult = true;
+                    //   verify
+                    // recorded xpath: //*[@id="logoutForm"]/div/div[2]
+                    webElement = browser.FindElement(By.XPath("//*[@id='logoutForm']/div/div[2]"));
+                    //   report
+                    stepResult = Helper.TestStepCompare(stepNumber, stepName, "(County User)", webElement.Text);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+                // STEP: click Log Off link 
+                // ---------------------------------------
+                if (!testAbort)
+                {
+                    //   prep
+                    stepNumber++;
+                    stepName = "Click 'Log Off'";
+                    stepResult = true;
+                    //   action
+                    //      Logoff button is hidden behind a popup menu
+                    browser.FindElement(By.PartialLinkText("Manage Account")).Click();
+                    browser.FindElement(By.PartialLinkText("Log Off")).Click();
+                    //   verify
+                    stepResult = Helper.TestStepContains(stepNumber, stepName, "BeeWhere California | Home", browser.Title);
+                    //   report
+                    Helper.TakeScreenshot(browser, testId, stepNumber);
+                    if (!stepResult)
+                    {
+                        testResult = false;
+                        testAbort = false;
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                stepResult = false;
+                testResult = false;
+                Helper.TakeScreenshot(browser, testId, stepNumber);
+                Helper.TestStepComment(ex.Message);
+                Helper.TestStepResult(stepNumber, stepName, stepResult);
+            }
+
+            // finish up this test case
+            // ------------------------
+            Helper.TestCaseResult(testId, testTitle, testResult);
+            // fail this test case if testResult has been set to FALSE
+            Assert.IsTrue(testResult);
+        }
+
 
         [ClassInitialize()]
         public static void SetupClass(TestContext testContext)
